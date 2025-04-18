b1f584d1f87bc039f459539bb81c2d43ce32183e
ramya-sivakumar-zemoso <ramya.sivakumar@zemosolabs.com>
2025-04-18 13:33:03 +0530

    Removed all console log statements from the assignment and added files of debouncing and storage assignment.

--- ES6-assignment.js
+++ ES6-assignment.js
@@ -3,32 +3,28 @@
 //                      return “Hi” + name;
 //            }
 
-const printName = (name) => "Hi"+ name ;
-console.log(printName(" Ramya"));
+const printName = (name) => 'Hi' + name;
 
 // Rewrite the following code using template literals
 // const printBill = (name, bill) => {
 //                      return “Hi “ + name + “, please pay: “ + bill;
 //            }
 
-const printBill = (name,bill) => `Hi ${name} , please pay: ${bill}.`
-console.log(printBill("Ramya",250))
+const printBill = (name, bill) => `Hi ${name} , please pay: ${bill}.`;
 
 // Modify the following code such that the object properties are destructured and logged.
 // const person = {
 //                       name: “Noam Chomsky”,
 //                       age: 92
 //             }
-           
+
 //            let name = person.name;
 //            let age = person.age;
 //            console.log(name);
 //            console.log(age);
 
 const person = {
-                      name: "Noam Chomsky",
-                      age: 92
-            }
-           let {name, age} = person;
-           console.log(name);
-           console.log(age);
+  name: 'Noam Chomsky',
+  age: 92,
+};
+let { name, age } = person;
