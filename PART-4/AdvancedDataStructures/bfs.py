6e73581e92c17865d1cd51653851dd4385b0eb8d
ramya-sivakumar-zemoso <ramya.sivakumar@zemosolabs.com>
2025-06-19 23:51:40 +0530

    POC-PART-4 Added files of the implementation of generators, decorators, dotenv and advanced data structures in python

---
+++ PART-4/AdvancedDataStructures/bfs.py
@@ -0,0 +1,38 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_edge(self, u, v):
+        if u not in self.adj_list:
+            self.adj_list[u] = []
+        if v not in self.adj_list:
+            self.adj_list[v] = []
+        self.adj_list[u].append(v)
+        self.adj_list[v].append(u)
+
+    def bfs(self, start):
+        visited = []         
+        queue = [start]        
+
+        print("BFS Traversal:", end=" ")
+
+        while len(queue) > 0:
+            vertex = queue[0] 
+            queue = queue[1:]
+
+            if vertex not in visited:
+                print(vertex, end=" ")
+                visited.append(vertex)
+                for neighbor in self.adj_list[vertex]:
+                    if neighbor not in visited and neighbor not in queue:
+                        queue.append(neighbor)
+
+g = Graph()
+g.add_edge('A', 'B')
+g.add_edge('A', 'C')
+g.add_edge('B', 'D')
+g.add_edge('C', 'E')
+g.add_edge('D', 'F')
+g.add_edge('E', 'F')
+
+g.bfs('A')
---
+++ PART-4/Generators/generators.py
@@ -0,0 +1,9 @@
+#generator
+def fibonacci(limit):
+    a, b = 0, 1
+    while a < limit:
+        yield a
+        a, b = b, a + b
+
+for num in fibonacci(10):
+    print(num)
