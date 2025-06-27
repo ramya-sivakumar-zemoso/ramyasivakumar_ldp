6835cd2445f3d3d785a140a1acfeeaf5c3282279
ramya-sivakumar-zemoso <ramya.sivakumar@zemosolabs.com>
2025-06-27 13:45:18 +0530

    Modified the encapsulation implementation with the suggested changes.

--- PART-2/OOPS/Encapsulation.py
+++ PART-2/OOPS/Encapsulation.py
@@ -1,12 +1,12 @@
 #Encapsulation
-class computer:
+class Computer:
     def __init__(self):
         self.__maxprice=900
     def sell(self):
         print("selling price:{}".format(self.__maxprice))
     def setMaxPrice(self,price):
         self.__maxprice=price
-c=computer()
+c=Computer()
 c.sell()
 c.__maxprice=1000
 c.sell()
