15f6bc5d52c97ef5ef257d4c4c6a775553d49fe5
ramya-sivakumar-zemoso <ramya.sivakumar@zemosolabs.com>
2025-08-05 14:33:43 +0530

    deletd extra copies of the file

--- CartManagementSystemImplementation/schema/cart_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class CartRequest(BaseModel):
-    product_id: str
-    selected_quantity: int
-
-class CartUpdateRequest(BaseModel):
-    selected_quantity: Optional[int] = None
